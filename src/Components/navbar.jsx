4d8ce7a6bbb15a1a334e6dc9b56e470a3fa379bb
ruff1ezzz <88177499+ruff1ezzz@users.noreply.github.com>
2023-05-03 21:26:50 -0700

    Update navbar.jsx

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -33,15 +33,8 @@ function NavbarAI() {
           <Nav className="me-auto">
             {/* This is the dropdown */}
             <NavDropdown title="ABOUT" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Our Mission</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Our Team</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="INIATIVES" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
