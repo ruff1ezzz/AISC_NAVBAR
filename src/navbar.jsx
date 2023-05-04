973b3cd88f933054a26d0601eb8cb9c2a60d3f2e
SadracSantacruz <sadrac.santacruz@gmail.com>
2023-05-03 17:14:55 -0700

    added images and modified navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -11,9 +11,8 @@ function NavbarAI() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            {/* This is the dropdown */}
+            <NavDropdown title="ABOUT" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -24,6 +23,20 @@ function NavbarAI() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
+            <NavDropdown title="INIATIVES" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            {/* This are the other normal links */}
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
